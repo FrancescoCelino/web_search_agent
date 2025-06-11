fd66854f47195fddee9248a451839bba30c1adf0
FrancescoCelino <celinofrancesco849@gmail.com>
2025-06-11 16:34:01 +0200

    completed demo script

--- web_search_demo.py
+++ web_search_demo.py
@@ -1,3 +1,8 @@
+'''
+based on
+https://blog.futuresmart.ai/langgraph-agent-with-rag-and-nl2sql
+'''
+
 import getpass
 import os
 from dotenv import load_dotenv
@@ -154,7 +159,7 @@ class SQLToolSchema(BaseModel):
 def nl2sql_tool(question):
    """Tool to Generate and Execute SQL Query to answer User Questions related to chinook DB"""
    print("INSIDE NL2SQL TOOL")
-   execute_query = QuerySQLDataBaseTool(db=db)
+   execute_query = QuerySQLDatabaseTool(db=db)
    write_query = create_sql_query_chain(llm, db)
 
    chain = (
@@ -169,3 +174,53 @@ def nl2sql_tool(question):
 question = "what is the meaning of life?"
 result = nl2sql_tool.invoke({"question": question})
 print(f"Answer: {result}")
+
+# let's combine the tools now
+
+tools = [web_search_tool, retriever_tool, nl2sql_tool]
+llm_with_tools = llm.bind_tools(tools)
+
+from typing import Annotated, TypedDict
+from langgraph.graph import StateGraph
+from langgraph.graph.message import add_messages
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+graph_builder = StateGraph(State)
+graph_builder.add_node("chatbot", chatbot)
+
+from langgraph.prebuilt import tools_condition, ToolNode
+
+tool_node = ToolNode(tools=[web_search_tool, retriever_tool, nl2sql_tool])
+graph_builder.add_node("tools", tool_node)
+
+
+
+
+graph_builder.add_conditional_edges("chatbot", tools_condition)
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.set_entry_point("chatbot")
+graph = graph_builder.compile()
+
+config = {"configurable": {"thread_id": "1"}}
+
+while True:
+    user_input = input("User: ")
+    if user_input.lower() in ["quit", "exit", "q"]:
+        print("Goodbye!")
+        break
+
+    for event in graph.stream({"messages": [("user", user_input)]}, config):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+from IPython.display import Image, display
+
+try:
+    display(Image(graph.get_graph().draw_mermaid_png()))
+except Exception:
+    print("Error generating graph visualization.")
